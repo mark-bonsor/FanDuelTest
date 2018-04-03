d767e10f54da6f8ec6397157d633e8ff163b642e
mark-bonsor <mark.bonsor@gmail.com>
2018-04-03 09:21:14 +0100

    Initial Commit

---
+++ FanDuelTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FanDuelTest
+//
+//  Created by mark.bonsor on 03/04/2018.
+//  Copyright © 2018 MiB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FanDuelTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FanDuelTest
+//
+//  Created by mark.bonsor on 03/04/2018.
+//  Copyright © 2018 MiB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
